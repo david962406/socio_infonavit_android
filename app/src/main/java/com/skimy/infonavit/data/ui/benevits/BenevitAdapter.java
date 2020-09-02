9a412bc99c541ae0e528221e5298ec07d343ab21
david962406 <david962406@gmaill.com>
2020-09-02 13:50:33 -0500

    Benevit adapter comentada y organizada

--- app/src/main/java/com/skimy/infonavit/data/ui/benevits/BenevitAdapter.java
+++ app/src/main/java/com/skimy/infonavit/data/ui/benevits/BenevitAdapter.java
@@ -21,35 +21,37 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class BenevitAdapter extends RecyclerView.Adapter <RecyclerView.ViewHolder>{
+public class BenevitAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     ArrayList<BenevitClass> listDatos;
     private static final int TYPE_VIEW_DATA_LOCKED = 0;
     private static final int TYPE_VIEW_DATA_UNLOCKED = 1;
-    private static final int TYPE_VIEW_SKELETON = 2;
-    public void setdata(ArrayList<BenevitClass> listDatos){
-        this.listDatos=listDatos;
+    private static final int TYPE_VIEW_SKELETON = 2;// 3 vistas para cada caso, lockes, unlocked, sin datos
+
+    public void setdata(ArrayList<BenevitClass> listDatos) {
+        this.listDatos = listDatos;
         notifyDataSetChanged();
-    }
+    }//settear información para ell adaptador
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if(viewType==TYPE_VIEW_DATA_UNLOCKED){
-        View view= LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.single_benevit_unlocked,parent,false);
-        return new beneVitUnlockedViewHolder(view);}
-        else if(viewType==TYPE_VIEW_DATA_LOCKED) {
-            View view= LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.single_benevit_locked,parent,false);
+        if (viewType == TYPE_VIEW_DATA_UNLOCKED) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.single_benevit_unlocked, parent, false);
+            return new beneVitUnlockedViewHolder(view);
+        } else if (viewType == TYPE_VIEW_DATA_LOCKED) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.single_benevit_locked, parent, false);
             return new beneVitLockedViewHolder(view);
-        }
-        else{
-            View view= LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.single_benevit_skeleton,parent,false);
+        } else {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.single_benevit_skeleton, parent, false);
             return new beneVitSkeletonViewHolder(view);
         }
 
-    }
-    public class beneVitUnlockedViewHolder extends RecyclerView.ViewHolder{
+    }//viewholder de donde heredaran los adapters
+
+    public class beneVitUnlockedViewHolder extends RecyclerView.ViewHolder {
         TextView dato;
         TextView region;
         TextView vence;
@@ -59,16 +61,17 @@ public class BenevitAdapter extends RecyclerView.Adapter <RecyclerView.ViewHolde
         public beneVitUnlockedViewHolder(@NonNull View itemView) {
 
             super(itemView);
-            dato=itemView.findViewById(R.id.description);
-            region=itemView.findViewById(R.id.region);
-            vence=itemView.findViewById(R.id.expire_date);
-            fondo=itemView.findViewById(R.id.image_bkg);
-            logo=itemView.findViewById(R.id.logo_benevit);
+            dato = itemView.findViewById(R.id.description);
+            region = itemView.findViewById(R.id.region);
+            vence = itemView.findViewById(R.id.expire_date);
+            fondo = itemView.findViewById(R.id.image_bkg);
+            logo = itemView.findViewById(R.id.logo_benevit);
         }
-        public void asignardatos(String descripción, String territorio,String expirationdate,String color, String Imagen) throws ParseException {
+
+        public void asignardatos(String descripción, String territorio, String expirationdate, String color, String Imagen) throws ParseException {
             dato.setText(descripción);
             region.setText(territorio);
-            vence.setText("Vence en: "+convertirDate(expirationdate)+" días");
+            vence.setText("Vence en: " + convertirDate(expirationdate) + " días");
             fondo.invalidate();
             fondo.setBackgroundColor(Color.parseColor(color));
             Glide.with(itemView.getContext())
@@ -76,85 +79,91 @@ public class BenevitAdapter extends RecyclerView.Adapter <RecyclerView.ViewHolde
                     .into(logo);
 
         }
+
         public String convertirDate(String Date) throws ParseException {
             java.util.Date c = Calendar.getInstance().getTime();
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
             Date date = df.parse(Date);
             String TodayDate = df.format(c);
-            long diff=(date.getTime()-c.getTime())/(1000 * 60 * 60 * 24);
+            long diff = (date.getTime() - c.getTime()) / (1000 * 60 * 60 * 24);
             String exirationDate = df.format(c);
 
             return (String.valueOf(diff));
 
         }
-    }
-    public class beneVitLockedViewHolder extends RecyclerView.ViewHolder{
+    }//adapter debenevit unlocked
+
+    public class beneVitLockedViewHolder extends RecyclerView.ViewHolder {
         ImageView promo;
+
         public beneVitLockedViewHolder(@NonNull View itemView) {
             super(itemView);
-            promo=itemView.findViewById(R.id.image_promo);
+            promo = itemView.findViewById(R.id.image_promo);
         }
+
         public void asignardatos(String imagen) {
             Glide.with(itemView.getContext())
                     .load(imagen)
                     .into(promo);
         }
 
-    }
-    public class beneVitSkeletonViewHolder extends RecyclerView.ViewHolder{
+    }//adapter de benevit locked
+
+    public class beneVitSkeletonViewHolder extends RecyclerView.ViewHolder {
         ImageView promo;
+
         public beneVitSkeletonViewHolder(@NonNull View itemView) {
             super(itemView);
 
         }
 
 
-    }
+    }//adapter de benevit vacio sin información
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if(listDatos.isEmpty()){
+        if (listDatos.isEmpty()) {
 
-        }
-        else{
-            if(getItemViewType(position)==TYPE_VIEW_DATA_UNLOCKED)
-            { beneVitUnlockedViewHolder holder1=(beneVitUnlockedViewHolder)holder;
-            try {
-                holder1.asignardatos(listDatos.get(position).getTitle()
-                        ,listDatos.get(position).getTerritories().get(0).getName(),
-                        listDatos.get(position).getExpirationDate(),
-                        listDatos.get(position).getPrimaryColor(),listDatos.get(position).getAlly().getMiniLogoFullPath());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }}
-            else{
-                beneVitLockedViewHolder holder1=(beneVitLockedViewHolder) holder;
+        } else {
+            if (getItemViewType(position) == TYPE_VIEW_DATA_UNLOCKED) {
+                beneVitUnlockedViewHolder holder1 = (beneVitUnlockedViewHolder) holder;
+                try {
+                    holder1.asignardatos(listDatos.get(position).getTitle()
+                            , listDatos.get(position).getTerritories().get(0).getName(),
+                            listDatos.get(position).getExpirationDate(),
+                            listDatos.get(position).getPrimaryColor(), listDatos.get(position).getAlly().getMiniLogoFullPath());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                beneVitLockedViewHolder holder1 = (beneVitLockedViewHolder) holder;
                 holder1.asignardatos(listDatos.get(position).getVectorFullPath());
             }
         }
 
         //getItemViewType(position);
-    }
+    }//lógica de acuerdo al tipo view holder a usar en cada vista
 
     @Override
     public int getItemViewType(int position) {
-        if(listDatos.isEmpty()){
+        if (listDatos.isEmpty()) {
             return TYPE_VIEW_SKELETON;
+        } else {
+            if (listDatos.get(position).getLocked()) {
+                return TYPE_VIEW_DATA_LOCKED;
+            } else {
+                return TYPE_VIEW_DATA_UNLOCKED;
+            }
         }
-        else{
-        if(listDatos.get(position).getLocked())
-        {return TYPE_VIEW_DATA_LOCKED;}
-        else{
-            return TYPE_VIEW_DATA_UNLOCKED;
-        }}
 
-    }
+    }//lógica para settear las vistas de acuerdo al caso
 
     @Override
     public int getItemCount() {
-        if(listDatos.size()==0){
+        if (listDatos.size() == 0) {
             return 5;
+        } else {
+            return listDatos.size();
         }
-        else {return listDatos.size();}
-    }
+    }//si no existe infromación crear 5 objetos vacios para el skeleton, de lo contrario devolver tamaño del array
 }
